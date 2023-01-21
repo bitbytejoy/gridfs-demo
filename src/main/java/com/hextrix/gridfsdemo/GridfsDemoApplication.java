ca1392fbe5322a4bfad40688f766fe606484bb17
bitbytejoy <bitbytejoy@gmail.com>
2023-01-21 12:49:52 +0100

    init: BIG BANG

---
+++ src/main/java/com/hextrix/gridfsdemo/GridfsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hextrix.gridfsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GridfsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GridfsDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hextrix/gridfsdemo/GridfsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hextrix.gridfsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GridfsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
